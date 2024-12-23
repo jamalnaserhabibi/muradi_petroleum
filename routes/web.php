c1a07feb73284b83536c83fdaccbb8a809615604
jamalnaserhabibi <jamalnaser.tech@gmail.com>
2024-12-23 21:54:07 +0500

    my work

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,15 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AdminController;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/admin/login', [AdminController::class, 'index'])->name('admin.login');
+Route::get('/admin/dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
+Route::get('/admin/form', [AdminController::class, 'form'])->name('admin.form');
+Route::get('/admin/table', [AdminController::class, 'table'])->name('admin.table');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
